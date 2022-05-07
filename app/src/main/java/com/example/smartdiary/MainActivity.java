b7c8da4ba3e04fac7dea6a8ec53f13ee2c15e3ae
IT20620202 <IT20620202@my.sliit.lk>
2022-05-08 01:03:20 +0530

    AllNotes

--- app/src/main/java/com/example/smartdiary/MainActivity.java
+++ app/src/main/java/com/example/smartdiary/MainActivity.java
@@ -60,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        GoNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,)
+            }
+        });
 
     }
 
