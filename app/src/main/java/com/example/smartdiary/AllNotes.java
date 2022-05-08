e63fb24cb6c52f4f01e3e468c555f6330bdc2207
IT20620202 <IT20620202@my.sliit.lk>
2022-05-08 12:45:36 +0530

    Update AllNotes.java

--- app/src/main/java/com/example/smartdiary/AllNotes.java
+++ app/src/main/java/com/example/smartdiary/AllNotes.java
@@ -3,12 +3,25 @@ package com.example.smartdiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class AllNotes extends AppCompatActivity {
+    private ProgressBar PBLoading;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_notes);
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Notes");
     }
 }
